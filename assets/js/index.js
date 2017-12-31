65418665ff1a223b090d528d0aef574e31d5da92
Ben Wolfe <wolfeben16@gmail.com>
2017-12-30 19:32:08 -0800

    Add responsive skill section

---
+++ assets/js/index.js
@@ -0,0 +1,146 @@
+
+window.onload = function() {
+  loadMenu();
+  typewrite();
+  loadSliders();
+};
+
+/* ----------------------------------------------------------------- MENU */
+
+var hamburgerBtn, hamburgerLines, 
+	dropdownMenu, menuItems, pageLink;
+function loadMenu() {
+	hamburgerBtn   = document.querySelector("#hamburger");
+	hamburgerLines = document.querySelectorAll("#hamburger span");
+	dropdownMenu   = document.querySelector(".main-menu");
+	menuItems      = document.querySelectorAll(".main-menu .menu-item");
+	pageLink       = document.querySelectorAll(".main-menu a");	
+
+	hamburgerBtn.addEventListener("click", function() {
+		toggleDropdown();
+	});
+
+	pageLink.forEach(function(el) {
+		el.addEventListener("click", function() {
+			toggleDropdown();
+		});
+	});
+}
+
+function toggleDropdown() {
+	hamburgerLines.forEach(function(el) {
+		el.classList.toggle("active");
+	});
+	menuItems.forEach(function(el) {
+		el.classList.toggle("show");
+	});
+	dropdownMenu.classList.toggle("show");
+}
+
+/* -------------------------------------------------- TYPEWRITE ANIMATION */
+
+function typewrite() {
+  var typeElements = document.getElementsByClassName('typewrite');
+  for (var i=0; i<typeElements.length; i++) {
+    var words = typeElements[i].getAttribute('data-words');
+    if (words) {
+      new Typewriter(typeElements[i], JSON.parse(words));
+    }
+  }
+}
+
+var Typewriter = function(el, words) {
+    this.words = words;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+Typewriter.prototype.tick = function() {
+
+    if (this.el.classList.contains("blink")) {
+      this.el.classList.toggle("blink");
+    }
+
+    var i = this.loopNum % this.words.length;
+    var fullTxt = this.words[i];
+
+    if (this.isDeleting) 
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    else 
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+
+    this.el.innerHTML = this.txt;
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { 
+        delta /= 2; 
+    }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+        this.el.classList.toggle("blink");
+
+    } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 1000;
+        this.el.classList.toggle("blink");
+    }
+
+    setTimeout(function() { 
+        that.tick(); 
+    }, delta);
+};
+
+/* ----------------------------------------------------- SLIDER ANIMATION */
+
+var sliders, skillTitles;
+function loadSliders(){
+	//sliders = document.querySelectorAll(".slider");
+	skillGroups = document.querySelectorAll(".skills");
+	window.addEventListener("scroll", checkViewport);
+	window.addEventListener("load", checkViewport);
+	// window.addEventListener("resize", );
+}
+
+function checkViewport() {
+	var competency, sliders, groupName, query;
+	if (skillGroups) {
+		skillGroups.forEach(function(el){
+			if (inViewport(el)) {
+				groupName = el.classList[1];
+				query = "."+groupName+" .slider";
+				sliders = document.querySelectorAll(query);
+	    	sliders.forEach(function(skill) {
+		      competency = skill.getAttribute("data-progress-percent");
+		      skill.style.width = competency + "%"
+	    	})
+			}
+		});		
+	}
+}
+
+function resetSliders() {
+	sliders.forEach(function(slider) {
+		slider.style.width = "0px";
+	});
+}
+
+/* ------------------------------------------------------------ UTILITIES */
+function inViewport(el) {
+	var rect = el.getBoundingClientRect();
+	return (
+		rect.top >= 0 && 
+		rect.left >= 0 &&
+		rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+		rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+		);
+}
+
